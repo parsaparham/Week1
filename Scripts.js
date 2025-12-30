486c45d030ee8e3dc45f7950095f7f82b7856828
parsaparham <jafari2002@gmail.com>
2025-12-31 03:17:16 +0330

    Day3-Constructor Objects

--- Scripts.js
+++ Scripts.js
@@ -1,64 +1,25 @@
-//objects
+//objects Constructor
 
-var person={
-    firstName:"Mohammad",
-    lastName:"jafari"
-}
-
-var person2=new Object();
-
-person2.phoneNmber=09132263154;
-person2.age=44;
-
-
-console.log(person2);
-
-//Using Functions in Objects
-
-var course={
-    title:"Javascript",
-    teacher:"Mohammad Jafari",
-    level:1,
-    isActive:true,
-    views:0,
-    updateView:function()
-    {
-        return ++course.views;
+function Course(title,teacher,level,isActive,views,updateView)
+{
+    this.title=title,
+    this.teacher=teacher,
+    this.level=level,
+    this.isActive=isActive,
+    this.views=views,
+    this.updateView=function (){
+        return ++this.views;
     }
 }
 
-console.log(course.views);
-course.updateView();
-console.log(course.views);
+var Course1=new Course("Javascript","Mohammad Jafari",1,true,0);
+var Course2=new Course("Python","Parsa Jafari",2,true,110);
+var Course3=new Course("PytHtml Csshon","Parham Jafari",3,true,220);
+
+console.log(Course1);
+console.log(Course2);
+console.log(Course3);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(Name);
-
-//window.alert();
 
