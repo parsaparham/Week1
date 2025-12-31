a7786a6df6d8b6163d18fb351288c93c77647425
parsaparham <jafari2002@gmail.com>
2025-12-31 04:16:37 +0330

    Day 4 - for in - vs - for of

--- Scripts.js
+++ Scripts.js
@@ -1,35 +1,18 @@
-//Loops : For
+//Loops : for of - vs - for in
 
 var cars=["Bens","BME","Toyota","KIA","Alfa Romeo"]; //Array
-
-console.log(cars);
-
-console.log("The Car Brand is: "+cars);
-
-console.log("The Car Brand is: "+cars[2]);
-
-for(let i=0;i<5;i++)
-{
-    console.log("The Car Brand is :"+cars[i]);
-}
-
-
-//For in-- in Arrays
-for (key in cars)
-{
-    console.log(key);//output=> name ProductYear color isABS
-    console.log(cars[key]); //Output=> BMWX3 2006 Blue true
-}
-
-
-//For in-- in Objects
 const car={name:"BMW X3",ProductYear:2006,color:"Blue",isABS:true}; //an Object
-for (key in car)
+
+for(value of cars) //در آرایه داریم امتحان می کنیم
 {
-    //console.log(key);//output=> name ProductYear color isABS
-   // console.log(car[key]); //Output=> BMWX3 2006 Blue true
+    console.log(value); //Output => Bens BMW Toyota KIA AlfaRomeo
 }
 
+// تفاوتش با بالا را ببینید
+for(value in cars) //در آرایه داریم امتحان میکنیم
+{
+    console.log(value); //Output =>  0 1 2 3 4 
+}
 
 
 
