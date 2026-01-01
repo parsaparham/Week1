9f741dc9bdb1dcc221796e0a037cbacd739c9e9c
parsaparham <jafari2002@gmail.com>
2026-01-01 16:05:24 +0330

    Day 5 - getElementById

--- Scripts.js
+++ Scripts.js
@@ -1,34 +1,14 @@
 
+document.getElementById("test").innerHTML="Hello DOM";
+
+const element=document.getElementById("test");
+
+element.innerHTML="Hello DOM2";
+
+element.innerHTML="Date "+Date();
 
 
 
-
-
-//Loops : break vs continue 
-
-//واکشی عناصر آرایه
-
-//break => اگر به یه مقداری رسید کلا قطع میشود
-
-const cars=["Benz","BMW","Toyota","Ford","NIssan","KIA","Land Cruize","Pegeout","Hyundai"];  //an Array
-
-for(let i=0 ; i<10 ; i++)
-{
-    if(i==5){
-        break;
-    }
-    console.log(cars[i]);
-}
-
-//Continue => اگر به یه مقداری رسید فقط آن مقدار را بیخیال میشود و بعد دوباره ادامه میدهد 
-
-for(let i=0 ; i<10 ; i++)
-{
-    if(i==6){
-        continue;
-    }
-    console.log(cars[i]);
-}
 
 
 
