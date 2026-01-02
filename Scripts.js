32f8808d655d528ec271d81c6c9cb70cd0f33734
parsaparham <jafari2002@gmail.com>
2026-01-02 05:55:06 +0330

    Day 5- querySelector & querySelectorAll

--- Scripts.js
+++ Scripts.js
@@ -1,13 +1,44 @@
 
-document.getElementById("test").innerHTML="Hello DOM";
+const element=document.querySelector(".main-title");
 
-const element=document.getElementById("test");
+element.innerHTML="I Take Your H2 Tag";
 
-element.innerHTML="Hello DOM2";
 
-element.innerHTML="Date "+Date();
+console.log(element);
 
 //
+
+const element2=document.querySelector(".promo-area").id;
+console.log(element2);
+//element2="myID";
+
+
+
+const element3=document.querySelector("#showcase").className;
+console.log(element3);
+
+const element4=document.querySelector("#showcase").classList;
+
+const element5=document.querySelector("#myID").classList;// an array
+const element6=document.querySelector("#myID").classList[0];// a index 0 fro an array
+element6="test";
+
+//Omportant : some of classes in class list are readonly so can not change them
+
+//But you can add a class in them
+document.querySelector("#myID").classList.add("test");
+//Or you can delete a class
+document.querySelector("#myID").classList.remove("centered");
+
+//One time add a class and one time delete a class
+document.querySelector("#myID").classList.toggle("three-reasons");
+
+
+
+
+
+
+
 
 
 
