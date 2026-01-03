5da8799f8f86e069d8d872e29b0e30e74e33876b
parsaparham <jafari2002@gmail.com>
2026-01-04 02:05:52 +0330

    Day 6 - work with attributes

--- Scripts.js
+++ Scripts.js
@@ -1,37 +1,28 @@
+//Changr Attributes with Java
 
-const element=document.querySelector(".main-title");
+//const element=document.querySelector("#showCcse").hasAttribute("href");  //=>false
+//const element1=document.querySelector("#showcase").hasAttribute("class");   //true
+//console.log(element1);
 
-element.innerHTML="I Take Your H2 Tag";
+//document.querySelector("#showcase").setAttribute("class","test");
+
+//document.querySelector("#test").removeAttribute("class");
+
+const ctaElement=document.querySelector(".cta a");
+
+console.log(ctaElement.attributes);
+
+if(ctaElement.hasAttribute("target"))
+{
+    console.log(ctaElement.getAttribute("target"));// target is a ataribute  in that says if the eleman is click oprn that href in a new tab
+}else
+{
+    ctaElement.setAttribute("target","_blank");
+}
 
 
-console.log(element);
-
-//
-
-const element2=document.querySelector(".promo-area").id;
-console.log(element2);
-//element2="myID";
 
 
-
-const element3=document.querySelector("#showcase").className;
-console.log(element3);
-
-const element4=document.querySelector("#showcase").classList;
-
-const element5=document.querySelector("#myID").classList;// an array
-const element6=document.querySelector("#myID").classList[0];// a index 0 fro an array
-element6="test";
-
-//Omportant : some of classes in class list are readonly so can not change them
-
-//But you can add a class in them
-document.querySelector("#myID").classList.add("test");
-//Or you can delete a class
-document.querySelector("#myID").classList.remove("centered");
-
-//One time add a class and one time delete a class
-document.querySelector("#myID").classList.toggle("three-reasons");
 
 
 
